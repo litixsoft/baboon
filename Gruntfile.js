8086301e5efa7c614b0a59d4986b242507524c59
Timo Liebetrau <t.liebetrau73@gmail.com>
2013-02-13 08:02:50 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project configuration.
+    //noinspection JSUnresolvedFunction,JSUnresolvedVariable
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*!\n' +
+            ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+            ' *\n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+            ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n' +
+            ' */\n\n',
+        jshint: {
+            files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+            junit: 'build/reports/jshint.xml',
+            checkstyle: 'build/reports/jshint_checkstyle.xml',
+            options: {
+                bitwise: true,
+                curly: true,
+                eqeqeq: true,
+                forin: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                noempty: true,
+                nonew: true,
+                undef: true,
+                unused: true,
+                strict: true,
+                trailing: true,
+                indent: 4,
+                quotmark: 'single',
+                boss: true,
+                eqnull: true,
+                es5: true,
+                loopfunc: true,
+                sub: true,
+                browser: true,
+                jquery: true,
+                node: true,
+                white: true,
+                globals: {
+                    exports: true,
+                    describe: true,
+                    it: true,
+                    expect: true
+                }
+            }
+        },
+        watch: {
+            files: '<%= jshint.files %>',
+            tasks: ['jshint:files']
+        },
+        jasmine_node: {
+            specNameMatcher: './*.spec', // load only specs containing specNameMatcher
+            projectRoot: 'test',
+            requirejs: false,
+            forceExit: true,
+            jUnit: {
+                report: true,
+                savePath: './build/reports/jasmine/',
+                useDotNotation: true,
+                consolidate: true
+            }
+        }
+    });
+
+    // Load tasks.
+    //noinspection JSUnresolvedFunction
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //noinspection JSUnresolvedFunction
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //noinspection JSUnresolvedFunction
+    grunt.loadNpmTasks('grunt-jasmine-node');
+
+    // Default task.
+    //noinspection JSUnresolvedFunction
+    grunt.registerTask('default', ['jshint:files', 'jasmine_node']);
+    grunt.registerTask('test', ['jasmine_node']);
+};
---
+++ tests/tests.spec.js
@@ -0,0 +1,10 @@
+describe('Baboon', function () {
+    'use strict';
+
+    it('test for baboon', function () {
+        var result = true;
+
+        expect(result).toBe(true);
+
+    });
+});
