6c56615c13bd65903e715f08b80afbad14475ce3
Timo Liebetrau <t.liebetrau73@gmail.com>
2013-02-14 21:23:50 +0100

    update

--- example/app.js
+++ example/app.js
@@ -2,6 +2,7 @@ var baboon = require('../lib/baboon')(__dirname),
     app = baboon.express.app,
     middleware = baboon.middleware,
     server = baboon.server,
+    config = baboon.config,
     httpRoutes = require('./routes/http');
 
 // routes
@@ -9,5 +10,6 @@ app.get('/', httpRoutes.index);
 app.get('/login', httpRoutes.login);
 app.get('/app', middleware.restricted, httpRoutes.app);
 app.get('/contact', httpRoutes.contact);
+
 
 server.start();
