680bde728ea1c77a11311e22f0555c03475845e6
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-03-16 11:57:21 +0100

    Rückbau des Async Baboon abgeschlossen

--- example/app.js
+++ example/app.js
@@ -1,42 +1,33 @@
 //noinspection JSUnresolvedVariable
-var baboon = require('../lib/baboon'),
-    httpRoutes = require('./routes/http');
+var baboon = require('../lib/baboon')(__dirname),
+    httpRoutes = require('./routes/http'),
+    app = baboon.express.app,
+    auth = baboon.middleware.auth,
+    server = baboon.server,
+    io = server.io,
+    config = server.config;
 
-//noinspection JSUnresolvedVariable
-baboon(__dirname, function (err, res) {
+// routes
+app.get('/', httpRoutes.index);
+app.get('/login', httpRoutes.login);
+app.post('/login', auth.login);
+app.get('/logout', auth.logout);
+app.get('/app', auth.restricted, httpRoutes.app);
+app.get('/contact', httpRoutes.contact);
+
+io.sockets.on('connection', function (socket) {
     'use strict';
 
-    if (err) {
-        console.error(err);
-        throw new Error(err);
-    }
-
-    var app = res.express.app,
-        auth = res.middleware.auth,
-        server = res.server,
-        io = server.io,
-        config = server.config;
-
-    // routes
-    app.get('/', httpRoutes.index);
-    app.get('/login', httpRoutes.login);
-    app.post('/login', auth.login);
-    app.get('/logout', auth.logout);
-    app.get('/app', auth.restricted, httpRoutes.app);
-    app.get('/contact', httpRoutes.contact);
-
-    io.sockets.on('connection', function (socket) {
-        socket.emit('news', { hello: 'world' });
-        socket.on('my other event', function (data) {
-            console.log(data);
-        });
-
-        setInterval(function () {
-            socket.emit('send:time', {
-                time: (new Date()).toString()
-            });
-        }, 5000);
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
     });
 
-    server.start();
+    setInterval(function () {
+        socket.emit('send:time', {
+            time: (new Date()).toString()
+        });
+    }, 5000);
 });
+
+server.start();
