8d05efd478b8893312c9996e723dbb3e4b1eaee2
Sven Bernstein <s.bernstein@litixsoft.de>
2013-04-23 23:14:15 +0200

    Pagination, Popover und Tabs geadded

--- example/client/app/enterprise/enterprise.js
+++ example/client/app/enterprise/enterprise.js
@@ -239,6 +239,35 @@ angular.module('enterprise', [
     }])
 
 /**
+ * Pagination Controller
+ */
+    .controller('PaginationDemoCtrl',['$scope',function ($scope) {
+
+        $scope.noOfPages = 7;
+        $scope.currentPage = 4;
+        $scope.maxSize = 5;
+
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
+    }])
+
+/**
+ * Popover Controller
+ */
+    .controller('PopoverDemoCtrl',['$scope',function ($scope) {
+
+        $scope.dynamicPopover = "Hello, World!";
+        $scope.dynamicPopoverText = "dynamic";
+        $scope.dynamicPopoverTitle = "Title";
+
+        $scope.popover = {
+            content: 'Nix neues',
+            title: 'Titel'
+        }
+    }])
+
+/**
  * Enterprise edit controller
  */
     .controller('DropdownCtrl', ['$scope', function ($scope) {
@@ -277,6 +306,34 @@ angular.module('enterprise', [
                 ]
             }
         ];
+    }])
+
+/**
+ * Tabs controller
+ */
+    .controller('TabsDemoCtrl', ['$scope',function ($scope) {
+
+        $scope.panes = [
+            { title:"Dynamic Title 1", content:"Dynamic content 1" },
+            { title:"Dynamic Title 2", content:"Dynamic content 2" }
+        ];
+
+        $scope.tabs = [
+            {
+                "title": "Home",
+                "content": "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica."
+            },
+            {
+                "title": "Profile",
+                "content": "Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee."
+            },
+            {
+                "title": "About",
+                "content": "Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade."
+            }
+        ];
+
+        $scope.tabs.activeTab = 1;
     }])
 
 
