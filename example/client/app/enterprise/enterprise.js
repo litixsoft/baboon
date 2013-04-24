71331fe595e7cb7d6fd7e6e1717b91f9121384e3
Sven Bernstein <s.bernstein@litixsoft.de>
2013-04-24 10:14:19 +0200

    grunt test Fehler korrigiert

--- example/client/app/enterprise/enterprise.js
+++ example/client/app/enterprise/enterprise.js
@@ -365,8 +365,7 @@ angular.module('enterprise', [
         $scope.selected = undefined;
         $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
-//        $scope.typeaheadFn = function(query) {
-        $scope.typeaheadFn = function() {
+        $scope.typeaheadFn = function(query) {
             return $.map($scope.typeahead, function(country) {
                 return country + '_1';
             });
