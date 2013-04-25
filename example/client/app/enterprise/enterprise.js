651b8c24ede4f3c2b46b6efa4c937232e622a015
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-04-25 21:06:08 +0200

    bwt-114 Socket und Routing auf dem Server, gelöst

--- example/client/app/enterprise/enterprise.js
+++ example/client/app/enterprise/enterprise.js
@@ -4,7 +4,6 @@ angular.module('enterprise', [
 /**
  * Enterprise config area
  */
-
     .config(function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'enterprise/enterprise.html', controller: 'enterpriseCtrl'});
         $routeProvider.when('/new', {templateUrl: 'enterprise/edit.html', controller: 'newCtrl'});
@@ -14,12 +13,10 @@ angular.module('enterprise', [
  * Enterprise controller
  */
     .controller('enterpriseCtrl', ['$scope', 'enterpriseCrew', function ($scope, enterpriseCrew) {
-//        $scope.alerts = [
-//            { type: 'error', msg: 'Oh snap! Change a few things up and try submitting again.' },
-//            { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-//        ];
 
-        $scope.enterpriseCrew = enterpriseCrew;
+        enterpriseCrew.getAll(function(data) {
+            $scope.enterpriseCrew = data;
+        });
 
         $scope.open = function () {
             $scope.shouldBeOpen = true;
@@ -36,34 +33,20 @@ angular.module('enterprise', [
             backdropFade: true,
             dialogFade:true
         };
-
     }])
-
-/**
- * Enterprise edit controller
- */
-    .controller('TypeaheadCtrl', ['$scope', function ($scope) {
-
-        $scope.selected = undefined;
-        $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-
-        $scope.typeaheadFn = function(query, callback) {
-           // $http.get('/stations/autocomplete?term='+query).success(function(stations) {
-            callback($scope.states); // This will automatically open the popup with retrieved results
-           // });
-        };
-    }])
-
-
-
 /**
  * Enterprise edit controller
  */
     .controller('editCtrl', ['$scope', '$location', '$routeParams', 'enterpriseCrew', function ($scope, $location, $routeParams, enterpriseCrew) {
 
-        $scope.person = enterpriseCrew[$routeParams.id];
+        enterpriseCrew.getById([$routeParams.id], function(data) {
+            $scope.person = data;
+        });
+
         $scope.save = function () {
-            $location.path('/');
+            enterpriseCrew.updateById($routeParams.id, $scope.person, function() {
+                $location.path('/');
+            });
         };
     }])
 /**
@@ -72,7 +55,8 @@ angular.module('enterprise', [
     .controller('newCtrl', ['$scope', '$location','enterpriseCrew', function ($scope, $location, enterpriseCrew) {
         $scope.person = {name: '', description: ''};
         $scope.save = function () {
-            enterpriseCrew.push($scope.person);
-            $location.path('/');
+            enterpriseCrew.create($scope.person, function() {
+                $location.path('/');
+            });
         };
     }]);
--- example/server/api/socket/app/enterprise.js
+++ example/server/api/socket/app/enterprise.js
@@ -1,1 +1,32 @@
+module.exports = function(socket, acl) {
+    'use strict';
 
+    var res = {},
+        base = require('../base'),
+        enterpriseMock = [
+            {name: 'Picard', description: 'Captain'},
+            {name: 'Riker', description: 'Number One'},
+            {name: 'Worf', description: 'Security'}
+        ];
+
+    res.getAll = function (data, callback) {
+        callback(enterpriseMock);
+    };
+
+    res.getById = function (data, callback) {
+        callback(enterpriseMock[data.id]);
+    };
+
+    res.updateById = function (data, callback) {
+        enterpriseMock[data.id] = data.person;
+        callback('update successfully..');
+    };
+
+    res.create = function (data, callback) {
+        enterpriseMock.push(data.person);
+        callback('create successfully..');
+    };
+
+    // register resources
+    base.register('enterprise', socket, acl, res);
+};
