6a889037782eebfbcb3ea02fcf6b0cd40612ce50
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-05-01 11:41:08 +0200

    prepare merge

---
+++ example/client/app/home/home.js
@@ -0,0 +1,9 @@
+angular.module('home', ['home.about'])
+
+// config home module
+.config(function ($routeProvider) {
+    $routeProvider.when('/', {templateUrl: 'home/home.html', controller: 'homeCtrl'});
+    $routeProvider.when('/home', {templateUrl: 'home/home.html', controller: 'homeCtrl'});
+})
+// home controller
+.controller('homeCtrl', ['$scope', function () {}]);
