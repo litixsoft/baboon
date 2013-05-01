9b962e518cc26ea4bf4b794dcf22b1e18ee5622d
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-05-01 18:21:37 +0200

    bwt-137 Umbau der Assets und Builds, move vendors to assets

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
                 // all vendor files that need to be copy.
                 files: [
                     // images from bootstrap
-                    {dest: 'build/dist/assets/core/img/', src : ['**'], expand: true, cwd: 'vendor/bootstrap/img/'}
+                    {dest: 'build/dist/assets/img/', src : ['**'], expand: true, cwd: 'vendor/bootstrap/img/'}
                 ]
             }
         },
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
                     base: 'client/app'
                 },
                 src: ['client/app/**/*.html'],
-                dest: 'build/dist/assets/core/js/app.tpl.js'
+                dest: 'build/dist/assets/js/app.tpl.js'
             },
             common: {
                 options: {
@@ -91,7 +91,7 @@ module.exports = function (grunt) {
                     base: 'client/common'
                 },
                 src: ['client/common/**/*.html'],
-                dest: 'build/dist/assets/core/js/common.tpl.js'
+                dest: 'build/dist/assets/js/common.tpl.js'
             }
         },
 
@@ -107,22 +107,22 @@ module.exports = function (grunt) {
             lib: {
                 files: {
                     // lib debug
-                    'build/dist/assets/core/js/lib.js': [
+                    'build/dist/assets/js/lib.js': [
                         'vendor/angular/angular.js',
                         'vendor/angular-ui-bootstrap/ui-bootstrap-tpls-0.3.0.js'
                     ],
                     // lib release
-                    'build/dist/assets/core/js/lib.min.js': [
+                    'build/dist/assets/js/lib.min.js': [
                         'vendor/angular/angular.min.js',
                         'vendor/angular-ui-bootstrap/ui-bootstrap-tpls-0.3.0.min.js'
                     ],
                     // libs debug
-                    'build/dist/assets/core/css/lib.css': [
+                    'build/dist/assets/css/lib.css': [
                         'vendor/bootstrap/css/bootstrap.css',
                         'vendor/bootstrap/css/bootstrap-responsive.css'
                     ],
                     // libs release
-                    'build/dist/assets/core/css/lib.min.css': [
+                    'build/dist/assets/css/lib.min.css': [
                         'vendor/bootstrap/css/bootstrap.min.css',
                         'vendor/bootstrap/css/bootstrap-responsive.min.css'
                     ]
@@ -133,20 +133,20 @@ module.exports = function (grunt) {
              */
             app: {
                 files: {
-                    'build/dist/assets/core/js/app.js': [
+                    'build/dist/assets/js/app.js': [
                         'client/app/module.prefix',
                         'client/app/**/*.js',
                         '!client/app/**/*.spec.js',
                         'client/app/module.suffix'
                     ],
-                    'build/dist/assets/core/css/app.css': [
+                    'build/dist/assets/css/app.css': [
                         'client/app/**/*.css'
                     ]
                 }
             },
             common: {
                 files: {
-                    'build/dist/assets/core/js/common.js': [
+                    'build/dist/assets/js/common.js': [
                         'client/common/common.prefix',
                         'client/common/**/*.js',
                         '!client/common/**/*.spec.js',
@@ -162,19 +162,19 @@ module.exports = function (grunt) {
 
         ngmin: {
             app: {
-                src: ['build/dist/assets/core/js/app.js'],
+                src: ['build/dist/assets/js/app.js'],
                 dest: 'build/tmp/app.js'
             },
             app_tpl: {
-                src: ['build/dist/assets/core/js/app.tpl.js'],
+                src: ['build/dist/assets/js/app.tpl.js'],
                 dest: 'build/tmp/app.tpl.js'
             },
             common: {
-                src: ['build/dist/assets/core/js/common.js'],
+                src: ['build/dist/assets/js/common.js'],
                 dest: 'build/tmp/common.js'
             },
             common_tpl: {
-                src: ['build/dist/assets/core/js/common.tpl.js'],
+                src: ['build/dist/assets/js/common.tpl.js'],
                 dest: 'build/tmp/common.tpl.js'
             }
         },
@@ -186,10 +186,10 @@ module.exports = function (grunt) {
         uglify: {
             target: {
                 files: {
-                    'build/dist/assets/core/js/app.min.js': 'build/tmp/app.js',
-                    'build/dist/assets/core/js/app.tpl.min.js': 'build/tmp/app.tpl.js',
-                    'build/dist/assets/core/js/common.min.js': 'build/tmp/common.js',
-                    'build/dist/assets/core/js/common.tpl.min.js': 'build/tmp/common.tpl.js'
+                    'build/dist/assets/js/app.min.js': 'build/tmp/app.js',
+                    'build/dist/assets/js/app.tpl.min.js': 'build/tmp/app.tpl.js',
+                    'build/dist/assets/js/common.min.js': 'build/tmp/common.js',
+                    'build/dist/assets/js/common.tpl.min.js': 'build/tmp/common.tpl.js'
                 }
             }
         },
@@ -201,7 +201,7 @@ module.exports = function (grunt) {
         cssmin: {
             target: {
                 files: {
-                    'build/dist/assets/core/css/app.min.css': ['build/dist/assets/core/css/app.css']
+                    'build/dist/assets/css/app.min.css': ['build/dist/assets/css/app.css']
                 }
             }
         },
