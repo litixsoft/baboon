55aea1509e122d0b8e760475b819ca2b7a49a65d
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-05-03 08:28:25 +0200

    bwt-145 Toplevel,clean after merge

--- example/client/apps/blog/blog.js
+++
@@ -1,9 +0,0 @@
-angular.module('blog', ['templates-blog', 'templates-common', 'ui.bootstrap'])
-    .config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(false);
-        $routeProvider.when('/', {templateUrl: 'blog.tpl.html', controller: 'mainCtrl'});
-        $routeProvider.when('/blog', {templateUrl: 'blog.tpl.html', controller: 'mainCtrl'});
-    })
-    .controller('mainCtrl', ['$scope', function ($scope) {
-        $scope.title = 'Litixsoft Blog';
-    }]);
