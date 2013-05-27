5d644a468293f135252d84600fe7959e7117b1d2
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2013-05-27 21:54:24 +0200

    test(example): add code coverage

---
+++ example/config/karma.all.conf.js
@@ -0,0 +1,27 @@
+basePath = '../';
+
+files = [
+    JASMINE,
+    JASMINE_ADAPTER,
+    'vendor/angular/angular.js',
+    'vendor/angular/angular-mocks.js',
+    'test/fixtures/mocks.js',
+    '../node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js',
+    'client/common/baboon/**/*.js',
+    'client/app/**/*.js'
+];
+
+preprocessors = {
+    'client/app/**/*.js': 'coverage'
+};
+
+reporters = ['progress'];
+
+coverageReporter = {
+    type : 'html',
+    dir : 'build/reports/coverage/client'
+};
+
+browsers = ['Chrome', 'Safari', 'Firefox', 'ChromeCanary'];
+
+singleRun = true;
