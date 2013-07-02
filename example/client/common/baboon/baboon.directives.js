bab0e58021110c62cd224862f8be0809d5e8fba9
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2013-07-02 13:02:24 +0200

    feat(baboon.directives): add float and integer directive

--- example/client/common/baboon/baboon.directives.js
+++ example/client/common/baboon/baboon.directives.js
@@ -109,4 +109,56 @@ angular.module('baboon.directives', [])
                 });
             }
         };
+    })
+    .directive('integer', function () {
+        var INTEGER_REGEXP = /^\-?\d*$/;
+
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (INTEGER_REGEXP.test(viewValue)) {
+                        // it is valid
+                        ctrl.$setValidity('integer', true);
+
+                        return parseInt(viewValue, 10);
+                    } else {
+                        // it is invalid, return undefined (no model update)
+                        ctrl.$setValidity('integer', false);
+
+                        return undefined;
+                    }
+                });
+            }
+        };
+    })
+
+    .directive('smartFloat', function () {
+        var FLOAT_REGEXP = /^\-?\d+((\.|\,)\d+)?$/;
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (FLOAT_REGEXP.test(viewValue)) {
+                        // it is valid
+                        ctrl.$setValidity('float', true);
+
+                        return typeof viewValue === 'number' ? viewValue : parseFloat(viewValue.replace(',', '.'));
+                    } else {
+                        // it is invalid, return undefined (no model update)
+                        ctrl.$setValidity('float', false);
+
+                        return undefined;
+                    }
+                });
+
+                ctrl.$formatters.unshift(function (modelValue) {
+                    if (modelValue) {
+                        modelValue = modelValue.toFixed(2).replace('.', ',');
+                    }
+
+                    return modelValue;
+                });
+            }
+        };
     });
