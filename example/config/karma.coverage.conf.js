c1b2f10499ac4a01da339865c8f3201907468117
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2013-08-19 22:22:12 +0200

    chore(karma): update to grunt-karma v0.6.0 (includes karma v0.10.0)

--- example/config/karma.coverage.conf.js
+++ example/config/karma.coverage.conf.js
@@ -1,27 +1,82 @@
-basePath = '../';
+module.exports = function (config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'node_modules/baboon-client/vendor/angular/angular.js',
-    'node_modules/baboon-client/vendor/angular/angular-mocks.js',
-    'node_modules/baboon-client/lib/**/*.js',
-    'test/fixtures/mocks.js',
-    'client/public/vendor/showdown/showdown.js',
-    'client/app/**/*.js'
-];
+        frameworks: ['jasmine'],
 
-preprocessors = {
-    'client/app/**/*.js': 'coverage'
+        // list of files / patterns to load in the browser
+        files: [
+            'node_modules/baboon-client/vendor/angular/angular.js',
+            'node_modules/baboon-client/vendor/angular/angular-mocks.js',
+            'node_modules/baboon-client/lib/**/*.js',
+            'test/fixtures/mocks.js',
+            'client/public/vendor/showdown/showdown.js',
+            'client/app/**/*.js'
+        ],
+
+        preprocessors: {
+            'client/app/**/*.js': ['coverage']
+        },
+
+        // use dots reporter, as travis terminal does not support escaping sequences
+        // possible values: 'dots', 'progress'
+        // CLI --reporters progress
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'build/reports/coverage/client'
+        },
+
+        // web server port
+        // CLI --port 9876
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        // CLI --colors --no-colors
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        // CLI --log-level debug
+        logLevel: config.LOG_INFO,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        // CLI --auto-watch --no-auto-watch
+        autoWatch: false,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        // CLI --browsers Chrome,Firefox,Safari
+        browsers: ['Chrome'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        // CLI --capture-timeout 5000
+        captureTimeout: 20000,
+
+        // Auto run tests on start (when browsers are captured) and exit
+        // CLI --single-run --no-single-run
+        singleRun: true,
+
+        // report which specs are slower than 500ms
+        // CLI --report-slower-than 500
+        reportSlowerThan: 500,
+
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-ie-launcher',
+            'karma-safari-launcher',
+            'karma-coverage',
+            'karma-junit-reporter'
+        ]
+    });
 };
-
-reporters = ['progress', 'coverage'];
-
-coverageReporter = {
-    type: 'html',
-    dir: 'build/reports/coverage/client'
-};
-
-browsers = ['Chrome'];
-
-singleRun = true;
