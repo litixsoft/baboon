73ac3efc91e1fcfdd0e92425243df43d81f3214f
Sven Bernstein <s.bernstein@litixsoft.de>
2013-08-22 10:47:36 +0200

    Dokumentation Umbau

--- example/client/app/doc/app.js
+++ example/client/app/doc/app.js
@@ -10,7 +10,7 @@ angular.module('baboon.documentation', [
     ])
     .config(['$routeProvider', '$locationProvider', '$translateProvider', function ($routeProvider, $locationProvider, $translateProvider) {
         $locationProvider.html5Mode(true);
-        $routeProvider.when('/doc', {templateUrl: '/doc/index.html'});
+        $routeProvider.when('/doc', {templateUrl: '/doc/index.html',controller: 'docCtrl'});
         $routeProvider.otherwise({redirectTo: '/doc'});
 
         $translateProvider.useStaticFilesLoader({
@@ -38,12 +38,56 @@ angular.module('baboon.documentation', [
             }
         });
     }])
-    .controller('rootCtrl', ['$rootScope', 'msgBox', function ($scope, msgBox) {
+    .directive('markdown', function () {
+        var converter = new Showdown.converter();
+
+        return {
+            restrict: 'E',
+            link: function (scope, element, attrs) {
+                scope.$watch(attrs.ngModel, function (value) {
+                    var htmlText = converter.makeHtml(value || '');
+                    element.html(htmlText);
+                });
+            }
+        };
+    })
+    .controller('rootCtrl', ['$rootScope', '$http', 'msgBox', function ($scope,$http, msgBox) {
 
         $scope.modal = msgBox.modal;
 
-        $scope.openMdLink = function(){
-            console.log("click");
+    }])
+    .controller('docCtrl', ['$rootScope', '$http', 'msgBox', function ($scope,$http, msgBox) {
+
+        $scope.linkList = [
+            {title:'Baboon Installation',route:'/doc/md/first'},
+            {title:'Baboon Dingens',route:'/doc/md/second'},
+            {title:'Baboon Super',route:'/doc/md/third',children:[
+            {title:'Super Doll',route:'/doc/md/quad',children:[
+                {title:'Doll 1',route:'/doc/md/five',icon:'home'},
+                {title:'Doll 2',route:'/doc/md/six',icon:'gear'},
+                {title:'Doll 3',route:'/doc/md/seven',icon:'home'}]
+            }]
+        }];
+
+        $scope.markCode = {content: 'loading...'};
+
+        $scope.openMdLink = function(path){
+            console.log("click: "+path);
+            $http({
+                url: [
+                    path,
+                    '.md'
+                ].join(''),
+                method: 'GET',
+                params: ''
+            }).success(function (data) {
+                    console.log(data.markdown);
+                    $scope.markCode.content = data.markdown;
+                    // deferred.resolve(data);
+                }).error(function (data) {
+                    console.log("Fehler");
+//                    deferred.reject(options.key);
+                });
         };
 
     }]);
