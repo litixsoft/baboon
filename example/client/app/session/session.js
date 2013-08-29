ce00b2b3d91bfc3aebbec30c0bb03d60dac2d725
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-08-29 15:51:25 +0200

    update session and example

--- example/client/app/session/session.js
+++ example/client/app/session/session.js
@@ -3,4 +3,28 @@ angular.module('sessionDoc', []).
     config(function ($routeProvider) {
         $routeProvider.when('/session', {templateUrl: '/session/session.html', controller: 'sessionDocCtrl'});
     }).
-    controller('sessionDocCtrl', ['$scope', function () {}]);
+    controller('sessionDocCtrl', ['$scope', 'session', '$log', function ($scope, session, $log) {
+
+        $scope.activityMsg = [];
+
+        $scope.getLastActivity = function() {
+            session.getLastActivity(function(err, data) {
+                if(err) {
+                    $log.error(err);
+                    $scope.activityMsg.push(err);
+                }
+                else {
+                    var now = new Date(data.activity);
+                    $scope.activityMsg.push('last activity is ' + now);
+                }
+            });
+        };
+        $scope.setActivity = function(){
+            var now = new Date();
+            $scope.activityMsg.push('set activity to ' + now);
+            session.setActivity();
+        };
+        $scope.clearActivityMsg = function() {
+            $scope.activityMsg = [];
+        };
+    }]);
