1b1c2aed0af4b94afe3868fd4ae5a932af27b67f
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2013-10-08 13:19:55 +0200

    refactor(example): clean up home/about.html

--- example/app.js
+++ example/app.js
@@ -1,20 +1,14 @@
 'use strict';
-//noinspection JSUnresolvedVariable
+
 var path = require('path'),
-    lxHelpers = require('lx-helpers'),
     baboon = require('../lib/baboon')(path.join(__dirname)),
     middleware = baboon.middleware,
     server = baboon.server,
-    app = server.app,
-    logging = baboon.logging,
-    rights = baboon.rights,
-    config = baboon.config;
-//    api = require(config.path.api);
+    app = server.app;
 
 ///////////////////////////////////////////
 // routes config
 ///////////////////////////////////////////
-
 
 // toplevel uiExamples route
 app.get('/ui', function (req, res) {
@@ -37,50 +31,6 @@ app.get('/admin', function (req, res) {
     middleware.session.checkSession(req, res, function () {
         middleware.context.index(req, res);
         res.render('admin/index');
-    });
-});
-
-// toplevel admin test route
-app.get('/admin/startAdministration', function (req, res) {
-    middleware.session.checkSession(req, res, function () {
-
-        rights.data().getExtendedAcl(req.session.user, ['Admin'], function (error, result) {
-            if (error) {
-                logging.syslog.error(error);
-                res.render('404');
-                return;
-            }
-
-            if (result) {
-                req.session.user.acl = result;
-
-                var navigation = config.path.navigation ? require(config.path.navigation) : null,
-                    userNavigation = rights.secureNavigation(req.session.user, navigation),
-                    topLevelNavigation = [];
-
-                // extract top level navigation links
-                lxHelpers.forEach(userNavigation, function (navItem) {
-                    var topLevelLink = lxHelpers.clone(navItem);
-                    delete topLevelLink.children;
-
-                    topLevelNavigation.push(topLevelLink);
-                });
-
-                // save navigation in session
-                req.session.navigation = userNavigation;
-
-                res.locals.isAuth = true;
-                res.locals.username = req.session.user.name;
-                res.locals.navigation = userNavigation;
-                res.locals.topLevelNavigation = topLevelNavigation;
-
-                res.render('admin/admin');
-                return;
-            }
-
-            middleware.context.index(req, res);
-            res.render('index');
-        });
     });
 });
 
