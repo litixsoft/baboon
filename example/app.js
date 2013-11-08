52039e204f6f611f1cfc326eff5a169519cecf23
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-11-08 10:42:01 +0100

    integrate passport-google, passport-github

--- example/app.js
+++ example/app.js
@@ -5,101 +5,66 @@ var baboon = require('../lib/baboon')(path.join(__dirname));
 var middleware = baboon.middleware;
 var server = baboon.server;
 var app = server.app;
+var rights = baboon.rights;
 var passport = require('passport');
-//var GoogleStrategy = require('passport-google').Strategy;
-//var GitHubStrategy = require('passport-github').Strategy;
+var GoogleStrategy = require('passport-google').Strategy;
+var GitHubStrategy = require('passport-github').Strategy;
 
 ///////////////////////////////////////////
 // auth strategies
 ///////////////////////////////////////////
 
-//passport.use(new GoogleStrategy({
-//        returnURL: process.env.GOOGLE_RETURN_URL || "http://127.0.0.1:3000/auth/google/return",
-//        realm: process.env.GOOGLE_REALM || "http://127.0.0.1:3000/"
-//    },
-//    function(identifier, profile, done) {
-//        findOrCreate(identifier, profile, function(err, user) {
-//
-//            console.log('### identifier');
-//            console.log(identifier);
-//
-//
-//            console.log('### profile');
-//            console.log(profile);
-//            done(err, user);
-//        });
-//    }
-//));
-//
-//passport.use(new GitHubStrategy({
-//        clientID: 'e255d4c484d293e40e05',
-//        clientSecret: 'c939429366715b49000c95bab6f0bdb4fe10c7be',
-//        callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-//    },
-//    function(accessToken, refreshToken, profile, done) {
-//        findOrCreate({accessToken:accessToken, refreshToken:refreshToken}, profile, function(err, user) {
-//            console.log('### accessTocken');
-//            console.log(accessToken);
-//
-//            console.log('### refreshToken');
-//            console.log(refreshToken);
-//
-//            console.log('### profile');
-//            console.log(profile);
-//            done(err, user);
-//        });
-//    }
-//));
+passport.use(new GoogleStrategy({
+        returnURL: process.env.GOOGLE_RETURN_URL || "http://127.0.0.1:3000/auth/google/return",
+        realm: process.env.GOOGLE_REALM || "http://127.0.0.1:3000/"
+    },
+    function(identifier, profile, done) {
+        rights.findOrCreateUser(profile.emails[0].value,profile.displayName, function(err, user) {
+            done(err, user);
+        });
+    }
+));
+
+passport.use(new GitHubStrategy({
+        clientID: 'e255d4c484d293e40e05',
+        clientSecret: 'c939429366715b49000c95bab6f0bdb4fe10c7be',
+        callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        rights.findOrCreateUser(profile.emails[0].value, profile.displayName, function (err, user) {
+            done(err, user);
+        });
+    }
+));
 
 ///////////////////////////////////////////
 // routes config
 ///////////////////////////////////////////
 
-//app.get('/auth/google', passport.authenticate('google'));
-//app.get('/auth/google/return', passport.authenticate('google', { session:false }),
-//    function(req, res) {
-//
-//        req.session.regenerate(function () {
-//            // set user
-//            req.session.user = req.user;
-//            delete req.user;
-//
-//            // set time for activity checking
-//            req.session.activity = new Date();
-//            req.session.start = new Date();
-//
-//            // set user language in session
-//            if (user.language) {
-//                req.session.language = user.language;
-//            }
-//
-//            // login successfully
-//            res.redirect('/');
-//        });
-//    });
-//
-//app.get('/auth/github', passport.authenticate('github'));
-//app.get('/auth/github/callback', passport.authenticate('github', { session:false }),
-//    function(req, res) {
-//
-//        req.session.regenerate(function () {
-//            // set user
-//            req.session.user = req.user;
-//            delete req.user;
-//
-//            // set time for activity checking
-//            req.session.activity = new Date();
-//            req.session.start = new Date();
-//
-//            // set user language in session
-//            if (user.language) {
-//                req.session.language = user.language;
-//            }
-//
-//            // login successfully
-//            res.redirect('/');
-//        });
-//    });
+app.get('/auth/google', passport.authenticate('google'));
+app.get('/auth/google/return', passport.authenticate('google', { session:false }),
+    function(req, res) {
+
+        // login success middleware
+        middleware.auth.loginSuccess(req, function(error, result) {
+            if (!error && result) {
+                res.redirect('/');
+            }
+        });
+    }
+);
+
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', { session:false }),
+    function(req, res) {
+        // login success middleware
+        middleware.auth.loginSuccess(req, function(error, result) {
+            if (!error && result) {
+                res.redirect('/');
+            }
+        });
+    }
+);
 
 
 // toplevel uiExamples route
