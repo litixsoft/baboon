aa333561a4d4e0cc08522fa703212aa01682424a
Timo Liebetrau <t.liebetrau73@gmail.com>
2014-01-12 21:39:04 +0100

    chore(core): reset core

--- example/client/app/home/home.js
+++
@@ -1,7 +0,0 @@
-/*global angular*/
-angular.module('home', ['home.about'])
-    // config home module
-    .config(function ($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'home/home.html'});
-        $routeProvider.when('/home', {templateUrl: 'home/home.html'});
-    });
--- example/client/modules/README.md
+++
@@ -1,1 +0,0 @@
-create your modules here
