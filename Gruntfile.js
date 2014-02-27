6525f0dceb4579d65b675ec610441c3c509e8eaf
lxNoodles <d.sachs@litixsoft.de>
2014-02-27 13:52:11 +0100

    feat(client): remove baboon client from project

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,9 +32,6 @@ module.exports = function (grunt) {
             jshint: {
                 files: [
                     'lib/**/*.js',
-                    '!lib/client/bower_components/**/*.js',
-                    /*'!lib/client/karma.conf.js',
-                    '!lib/client/karma.coverage.conf.js',*/
                     'test/**/*.js',
                     'Gruntfile.js'
                 ]
@@ -45,11 +42,7 @@ module.exports = function (grunt) {
             lint: ['.reports/lint'],
             coverage: ['.reports/coverage', '.tmp'],
             node_modules: ['node_modules'],
-            ci: ['.reports', '.tmp'],
-            coverage_client: '.reports/coverage/client',
-            coverage_lib: '.reports/coverage/lib',
-            test: '.reports/test',
-            jshint: '.reports/jshint'
+            ci: ['.reports', '.tmp']
         },
         jshint: {
             options: {
@@ -78,10 +71,10 @@ module.exports = function (grunt) {
         },
         bgShell: {
             coverage: {
-                cmd: 'node node_modules/istanbul/lib/cli.js cover --dir .reports/coverage/lib node_modules/grunt-jasmine-node/node_modules/jasmine-node/bin/jasmine-node -- test --forceexit'
+                cmd: 'node node_modules/istanbul/lib/cli.js cover --dir .reports/coverage node_modules/grunt-jasmine-node/node_modules/jasmine-node/bin/jasmine-node -- test --forceexit'
             },
             cobertura: {
-                cmd: 'node node_modules/istanbul/lib/cli.js report --root .reports/coverage/lib --dir .reports/coverage/lib cobertura'
+                cmd: 'node node_modules/istanbul/lib/cli.js report --root .reports/coverage --dir .reports/coverage cobertura'
             },
             npm: {
                 cmd: 'npm install',
@@ -89,14 +82,9 @@ module.exports = function (grunt) {
             }
         },
         open: {
-            coverageClient: {
+            coverage: {
                 path: function () {
-                    return path.join(__dirname, getCoverageReport('.reports/coverage/client/'));
-                }
-            },
-            coverageServer: {
-                path: function () {
-                    return path.join(__dirname, getCoverageReport('.reports/coverage/lib/'));
+                    return path.join(__dirname, getCoverageReport('.reports/coverage/'));
                 }
             }
         },
@@ -107,83 +95,17 @@ module.exports = function (grunt) {
             forceExit: true,
             jUnit: {
                 report: true,
-                savePath: '.reports/test/lib/',
+                savePath: '.reports/test/',
                 useDotNotation: true,
                 consolidate: true
-            }
-        },
-        karma: {
-            unit: {
-                configFile: 'lib/client/karma.conf.js',
-                singleRun: true
-            },
-            coverage: {
-                configFile: 'lib/client/karma.coverage.conf.js'
-            },
-            ci: {
-                configFile: 'lib/client/karma.conf.js',
-                reporters: ['mocha', 'junit'],
-                junitReporter: {
-                    outputFile: '.reports/test/client/karma.xml',
-                    suite: 'karma'
-                }
-            },
-            cobertura: {
-                configFile: 'lib/client/karma.coverage.conf.js',
-                coverageReporter: {
-                    type: 'cobertura',
-                    dir: '.reports/coverage/client'
-                }
             }
         }
     });
 
     grunt.registerTask('lint', ['jshint:test']);
-    grunt.registerTask('test:lib', ['clean:test', 'jshint:test', 'jasmine_node']);
-    grunt.registerTask('test:client', ['clean:test', 'jshint:test', 'karma:unit']);
-    grunt.registerTask('test', [
-        'clean:jasmine',
-        'jshint:test',
-        'jasmine_node',
-        'karma:unit'
-    ]);
-
-    grunt.registerTask('ci', [
-        'clean:test',
-        'clean:coverage_lib',
-        'clean:coverage_client',
-        'clean:test',
-        'clean:jshint',
-        'jshint:jslint',
-        'jshint:checkstyle',
-        'jasmine_node',
-        'bgShell:coverage',
-        'bgShell:cobertura',
-        'karma:ci',
-        'karma:coverage',
-        'karma:cobertura'
-    ]);
-
-    // coverage client
-    grunt.registerTask('cover:client', [
-        'clean:coverage_client',
-        'karma:coverage',
-        'open:coverageClient'
-    ]);
-
-    // coverage server
-    grunt.registerTask('cover:lib', [
-        'clean:coverage_lib',
-        'bgShell:coverage',
-        'open:coverageServer'
-    ]);
-
-    // coverage all
-    grunt.registerTask('cover', [
-        'cover:client',
-        'cover:lib'
-    ]);
-
+    grunt.registerTask('test', ['clean:jasmine', 'jshint:test', 'jasmine_node']);
+    grunt.registerTask('cover', ['clean:coverage', 'jshint:test', 'bgShell:coverage', 'open:coverage']);
+    grunt.registerTask('ci', ['clean:ci', 'jshint:jslint', 'jshint:checkstyle', 'jasmine_node', 'bgShell:coverage', 'bgShell:cobertura']);
     grunt.registerTask('update', 'Delete node_modules folder and run npm install', ['clean:node_modules', 'bgShell:npm']);
 
     // Default task.
