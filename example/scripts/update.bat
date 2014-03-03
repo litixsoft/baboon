8468a8ef68715c3bb21686b9afcc1670b821644f
Thomas Scheibe <th.scheibe@litixsoft.de>
2014-03-03 14:15:09 +0100

    Update script for windows.

--- example/scripts/update.bat
+++ example/scripts/update.bat
@@ -1,9 +1,9 @@
-@echo OFF
-
-if exist node_modules rd /s /q node_modules
-if exist client/assets/bower_components rd /s /q bower_components
+@echo off
+pushd "%~dp0\.."
+if exist "node_modules" rd /s /q "node_modules"
+if exist "client\assets\bower_components" rd /s /q "client\assets\bower_components"
 
 call npm install
 call bower install
 
-pause
+popd
