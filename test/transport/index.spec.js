2a27a24159e6b5b7a9c7b9ea30f4889db0ed0044
marco kannenberg <sharkboy1976@googlemail.com>
2014-03-13 13:10:18 +0100

    test(transport): started tests

--- test/transport/index.spec.js
+++ test/transport/index.spec.js
@@ -1,11 +1,36 @@
-'use strict';
+/*global describe, it, expect*/
+describe('transport/index', function () {
+    'use strict';
 
-describe('test', function() {
+// Module dependencies.
+    var stdio = require('stdio');
 
-//    var foo = require('../../server/controllers/foo');
-//
-//    it('should be true', function () {
-//        var sut = foo.test('timo');
-//        expect('hallo timo').toBe(sut.test);
-//    });
+// Set command line arguments
+    var argv = stdio.getopt({
+        'config': {key: 'c', args:1, description: 'Use the specified config section'},
+        'port': {args:1, description: 'Use the specified port'},
+        'protocol': {args:1, description: 'Use the specified protocol'},
+        'livereload': {description: 'Use livereload snippet for client'}
+    });
+
+    var path = require('path');
+    var rootPath = path.resolve(__dirname, '../', '../', 'lib');
+    var baboon = require(path.resolve(rootPath, 'baboon'))(rootPath, argv);
+
+    var transport = require(path.resolve(rootPath, 'transport', 'index'));
+
+    it('should throw an Error when not given params', function () {
+        var func = function () {
+            return transport();
+        };
+        expect(func).toThrow();
+    });
+
+    it('should throw an Error when not given param options', function () {
+        baboon.conig = null;
+        var func = function () {
+            return transport(baboon);
+        };
+        expect(func).toThrow();
+    });
 });
