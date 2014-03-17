0f0eb7e643e62b7083108df83df964df93d303e9
lxNoodles <d.sachs@litixsoft.de>
2014-03-17 14:27:03 +0100

    refactor(navigation): directives for navigation - add directives - remove controller and templates for ngInclude

--- example/client/common/navigation/navigation.js
+++ example/client/common/navigation/navigation.js
@@ -1,36 +1,77 @@
 'use strict';
 
 angular.module('common.navigation', ['bbc.navigation'])
-    .controller('CommonNavigationCtrl', function ($scope, $location, $bbcNavigation) {
+    .directive('navigationTop', function($bbcNavigation, $location) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<ul class="nav nav-pills">' +
+                           '<li ng-repeat="item in menuTopList" ng-class="{active: isActive(item.route)}">' +
+                                '<a ng-if="item.target" ng-href="{{item.route}}" target="{{item.target}}">{{item.title | translate }}</a>' +
+                                '<a ng-if="!item.target" ng-href="{{item.route}}">{{item.title | translate }}</a>' +
+                           '</li>' +
+                        '</ul>',
+            scope: {
+                orientation: '@'
+            },
+            link: function (scope, element) {
+                if(scope.orientation === 'vertical') {
+                    element.addClass('nav-stacked');
+                }
 
-        var appRoute = $bbcNavigation.getRoute();
+                $bbcNavigation.getTopList(function (error, navList) {
+                    if (error || navList.length === 0) {
+                        scope.menuTopList = [];
+                    }
+                    else {
+                        scope.menuTopList = navList;
+                    }
+                });
 
-        $bbcNavigation.getTopList(function (error, navList) {
+                scope.isActive = function (route) {
+                    if (route === $bbcNavigation.getRoute()) {
+                        return true;
+                    }
 
-            if (error || navList.length === 0) {
-                $scope.menuTopList = [];
+                    return route === $location.path();
+                };
             }
-            else {
-                $scope.menuTopList = navList;
+        };
+    })
+    .directive('navigationSub', function($bbcNavigation, $location) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<ul class="nav nav-pills nav-stacked">' +
+                           '<li ng-repeat="item in menuSubList" ng-class="{active: isActive(item.route)}">' +
+                                '<a ng-if="item.target" ng-href="{{item.route}}" target="{{item.target}}">{{item.title | translate }}</a>' +
+                                '<a ng-if="!item.target" ng-href="{{item.route}}">{{item.title | translate }}</a>' +
+                           '</li>' +
+                      '</ul>',
+            scope: {
+                orientation: '@'
+            },
+            link: function (scope, element) {
+                if(scope.orientation === 'horizontal') {
+                    element.removeClass('nav-stacked');
+                }
+
+                $bbcNavigation.getSubList(function (error, navList) {
+                    if (error || navList.length === 0) {
+                        scope.menuSubList = [];
+                    }
+                    else {
+                        scope.menuSubList = navList;
+                    }
+                });
+
+                scope.isActive = function (route) {
+                    if (route === $bbcNavigation.getRoute()) {
+                        return true;
+                    }
+
+                    return route === $location.path();
+                };
             }
-        });
-
-        $bbcNavigation.getSubList(function (error, navList) {
-
-            if (error || navList.length === 0) {
-                $scope.menuSubList = [];
-            }
-            else {
-                $scope.menuSubList = navList;
-            }
-        });
-
-        $scope.isActive = function (route) {
-
-            if (route === appRoute) {
-                return true;
-            }
-
-            return route === $location.path();
         };
     });
--- example/client/common/navigation/navigation.spec.js
+++ example/client/common/navigation/navigation.spec.js
@@ -1,32 +1,23 @@
 'use strict';
 
 describe('Common: common.nav', function () {
+    var navigationMockTop = [
+        { title: 'TEST', route: '/test', app: 'unitTest' }
+    ];
+    var navigationMockSub = [
+        { title: 'TEST-SUB', route: '/test-sub', app: 'unitTest' }
+    ];
 
-    describe('Controller: CommonNavigationCtrl', function () {
-
+    describe('navigationTop', function(){
         beforeEach(module('bbc.transport'));
         beforeEach(module('common.navigation'));
+        beforeEach(module('pascalprecht.translate'));
 
-        var $navigation, $scope, $ctrl, location;
-
-        var navigationMockTop = [
-            {
-                title: 'TEST',
-                route: '/test',
-                app: 'unitTest'
-            }
-        ];
-        var navigationMockSub = [
-            {
-                title: 'TEST-SUB',
-                route: '/test-sub',
-                app: 'unitTest'
-            }
-        ];
+        var $navigation, $scope, location, element, compile;
 
         beforeEach(function (done) {
-            inject(function ($controller, $rootScope, $injector, $location) {
-
+            inject(function ($compile, $rootScope, $injector, $location) {
+                compile = $compile;
                 $navigation = $injector.get('$bbcNavigation');
                 location = $location;
                 $navigation.getRoute = function() {
@@ -35,94 +26,164 @@ describe('Common: common.nav', function () {
                 $navigation.getTopList = function(callback) {
                     callback(null, navigationMockTop);
                 };
+
+                $scope = $rootScope.$new();
+                element = angular.element('<navigation-top></navigation-top>');
+                compile(element)($scope);
+                done();
+            });
+        });
+
+        it('should be correct initialized', function () {
+            var elementScope = element.isolateScope();
+
+            expect(elementScope.menuTopList).toBeDefined();
+            expect(elementScope.menuTopList.length).toBe(1);
+        });
+
+        it('should attach menus to the scope', function () {
+            var elementScope = element.isolateScope();
+
+            expect(elementScope.menuTopList.length).toBeGreaterThan(0);
+            expect(elementScope.menuTopList[0].title).toBe('TEST');
+            expect(elementScope.menuTopList[0].route).toBe('/test');
+            expect(elementScope.menuTopList[0].app).toBe('unitTest');
+        });
+
+        it('should active the correct location', function () {
+            var elementScope = element.isolateScope();
+
+            location.path('/test/path');
+            expect(elementScope.isActive('/test')).toBe(true);
+            expect(elementScope.isActive('/test/path')).toBe(true);
+            expect(elementScope.isActive('/test/foo')).toBe(false);
+        });
+
+        it('should not have a class nav-stacked', function() {
+            expect(element.hasClass('nav-stacked')).toBe(false);
+        });
+
+        it('should add a class by orientation', function() {
+            element = angular.element('<navigation-top orientation="vertical"></navigation-top>');
+            compile(element)($scope);
+            expect(element.hasClass('nav-stacked')).toBe(true);
+        });
+    });
+
+    describe('navigationTop with error', function() {
+        beforeEach(module('bbc.transport'));
+        beforeEach(module('common.navigation'));
+        beforeEach(module('pascalprecht.translate'));
+
+        var $navigation, $scope, element;
+
+        beforeEach(function (done) {
+            inject(function ($compile, $rootScope, $injector) {
+                $navigation = $injector.get('$bbcNavigation');
+                $navigation.getTopList = function(callback) {
+                    callback('error');
+                };
+
+                $scope = $rootScope.$new();
+                element = angular.element('<navigation-top></navigation-top>');
+                $compile(element)($scope);
+                done();
+            });
+        });
+
+        it('should be attach a empty menuTopList', function () {
+            var elementScope = element.isolateScope();
+
+            expect(elementScope.menuTopList.length).toBe(0);
+        });
+    });
+
+    describe('navigationSub', function(){
+        beforeEach(module('bbc.transport'));
+        beforeEach(module('common.navigation'));
+        beforeEach(module('pascalprecht.translate'));
+
+        var $navigation, $scope, location, element, compile;
+
+        beforeEach(function (done) {
+            inject(function ($compile, $rootScope, $injector, $location) {
+                compile = $compile;
+                $navigation = $injector.get('$bbcNavigation');
+                location = $location;
+                $navigation.getRoute = function() {
+                    return('/test-sub');
+                };
                 $navigation.getSubList = function(callback) {
                     callback(null, navigationMockSub);
                 };
 
                 $scope = $rootScope.$new();
-                $ctrl = $controller('CommonNavigationCtrl', {$scope: $scope});
+                element = angular.element('<navigation-sub></navigation-sub>');
+                compile(element)($scope);
                 done();
             });
         });
 
-        it('should active the correct location', function () {
+        it('should be correct initialized', function () {
+            var elementScope = element.isolateScope();
 
-            location.path('/test/path');
-            expect($scope.isActive('/test')).toBe(true);
-            expect($scope.isActive('/test/path')).toBe(true);
-            expect($scope.isActive('/test/foo')).toBe(false);
+            expect(elementScope.menuSubList).toBeDefined();
+            expect(elementScope.menuSubList.length).toBe(1);
         });
 
         it('should attach menus to the scope', function () {
-            expect($scope.menuTopList.length).toBeGreaterThan(0);
-            expect($scope.menuTopList[0].title).toBe('TEST');
-            expect($scope.menuTopList[0].route).toBe('/test');
-            expect($scope.menuTopList[0].app).toBe('unitTest');
+            var elementScope = element.isolateScope();
 
-            expect($scope.menuSubList.length).toBeGreaterThan(0);
-            expect($scope.menuSubList[0].title).toBe('TEST-SUB');
-            expect($scope.menuSubList[0].route).toBe('/test-sub');
-            expect($scope.menuSubList[0].app).toBe('unitTest');
+            expect(elementScope.menuSubList.length).toBeGreaterThan(0);
+            expect(elementScope.menuSubList[0].title).toBe('TEST-SUB');
+            expect(elementScope.menuSubList[0].route).toBe('/test-sub');
+            expect(elementScope.menuSubList[0].app).toBe('unitTest');
         });
 
-        describe('test error in menuTopList', function() {
+        it('should active the correct location', function () {
+            var elementScope = element.isolateScope();
 
-            beforeEach(function (done) {
-                inject(function ($controller, $rootScope, $injector) {
+            location.path('/test-sub/path');
+            expect(elementScope.isActive('/test-sub')).toBe(true);
+            expect(elementScope.isActive('/test-sub/path')).toBe(true);
+            expect(elementScope.isActive('/test-sub/foo')).toBe(false);
+        });
 
-                    $navigation = $injector.get('$bbcNavigation');
-                    $navigation.getTopList = function(callback) {
-                        callback('error');
-                    };
-                    $navigation.getSubList = function(callback) {
-                        callback(null, navigationMockSub);
-                    };
+        it('should have a class nav-stacked', function() {
+            expect(element.hasClass('nav-stacked')).toBe(true);
+        });
 
-                    $scope = $rootScope.$new();
-                    $ctrl = $controller('CommonNavigationCtrl', {$scope: $scope});
-                    done();
-                });
-            });
+        it('should remove a class by orientation', function() {
+            element = angular.element('<navigation-sub orientation="horizontal"></navigation-sub>');
+            compile(element)($scope);
+            expect(element.hasClass('nav-stacked')).toBe(false);
+        });
+    });
 
-            it('should be attach a empty menuTopList and an correct menuSubList', function () {
+    describe('navigationSub with error', function() {
+        beforeEach(module('bbc.transport'));
+        beforeEach(module('common.navigation'));
+        beforeEach(module('pascalprecht.translate'));
 
-                expect($scope.menuTopList.length).toBe(0);
+        var $navigation, $scope, element;
 
-                expect($scope.menuSubList.length).toBeGreaterThan(0);
-                expect($scope.menuSubList[0].title).toBe('TEST-SUB');
-                expect($scope.menuSubList[0].route).toBe('/test-sub');
-                expect($scope.menuSubList[0].app).toBe('unitTest');
+        beforeEach(function (done) {
+            inject(function ($compile, $rootScope, $injector) {
+                $navigation = $injector.get('$bbcNavigation');
+                $navigation.getSubList = function(callback) {
+                    callback('error');
+                };
+
+                $scope = $rootScope.$new();
+                element = angular.element('<navigation-sub></navigation-sub>');
+                $compile(element)($scope);
+                done();
             });
         });
 
-        describe('test error in menuSubList', function() {
-
-            beforeEach(function (done) {
-                inject(function ($controller, $rootScope, $injector) {
-
-                    $navigation = $injector.get('$bbcNavigation');
-                    $navigation.getTopList = function(callback) {
-                        callback(null, navigationMockTop);
-                    };
-                    $navigation.getSubList = function(callback) {
-                        callback('error');
-                    };
-
-                    $scope = $rootScope.$new();
-                    $ctrl = $controller('CommonNavigationCtrl', {$scope: $scope});
-                    done();
-                });
-            });
-
-            it('should be attach a empty menuSubList and an correct menuTopList', function () {
-
-                expect($scope.menuSubList.length).toBe(0);
-
-                expect($scope.menuTopList.length).toBeGreaterThan(0);
-                expect($scope.menuTopList[0].title).toBe('TEST');
-                expect($scope.menuTopList[0].route).toBe('/test');
-                expect($scope.menuTopList[0].app).toBe('unitTest');
-            });
+        it('should be attach a empty menuSubList', function () {
+            var elementScope = element.isolateScope();
+            expect(elementScope.menuSubList.length).toBe(0);
         });
     });
 });
