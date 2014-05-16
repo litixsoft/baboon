b25339e09cef4b8276b531e06eb452569b86012f
Timo Liebetrau <t.liebetrau73@gmail.com>
2014-05-16 23:02:48 +0200

    chore: update - login socket bugfix - gitignore

--- example/server/routes/index.js
+++ example/server/routes/index.js
@@ -7,11 +7,11 @@ module.exports = function (baboon) {
     var router = express.Router();
     var auth = baboon.middleware.auth;
 
-    router.get('/demo', auth.restrictedUser, function (req, res) {
+    router.get('/demo', auth.restricted, function (req, res) {
         res.render('app/demo/index');
     });
 
-    router.get('/demo/*', auth.restrictedUser, function (req, res) {
+    router.get('/demo/*', auth.restricted, function (req, res) {
         res.render('app/demo/index');
     });
 
@@ -23,7 +23,7 @@ module.exports = function (baboon) {
         res.render('app/admin/index');
     });
 
-    router.get('/account/*', auth.restrictedAdmin, function (req, res) {
+    router.get('/account/*', function (req, res) {
         res.render('app/account/index');
     });
 
