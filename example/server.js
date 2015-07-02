124819ccec128310e3b85ecfdfe5bbb2b1996128
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2015-07-02 14:14:22 +0200

    feat: add callback to baboon.appListen()
    callback will be called after the server is successfully started

--- example/server.js
+++ example/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var express = require('express');
+var chalk = require('chalk');
 var rootPath = __dirname;
-
 var baboon = require('../lib/baboon')(rootPath);
 var routes = require('../lib/routes')(baboon);
 
@@ -12,14 +12,51 @@ var app = express();
 // Express configuration
 require('../lib/config/express')(app, routes, baboon);
 
-// Express server
-var server = baboon.appListen(app);
+var server;
 
-// Socket.Io server
-var io = require('socket.io').listen(server);
+/**
+ * Gets called after the server is successfully started
+ */
+function serverStartedCallback () {
+    var config = baboon.config;
+    var startOn = 'server has been started on: ' + config.protocol + '://' + config.host + ':' + config.port;
+    var startMode = 'server has been started in ' + config.node_env + ' mode';
 
-// Socket.Io configuration
-require('../lib/config/socket-io')(io, baboon);
+    baboon.loggers.syslog.info(startOn);
+    baboon.loggers.syslog.debug(startMode);
+
+    if (config.logging.loggers.syslog.appender !== 'console') {
+        console.log(chalk.cyan('   info  - ') + startOn);
+    }
+
+    // Socket.Io server
+    var io = require('socket.io').listen(server);
+
+    // Socket.Io configuration
+    require('../lib/config/socket-io')(io, baboon);
+}
+
+/**
+ * Start the web server
+ */
+function startServer () {
+    server = baboon.appListen(app, serverStartedCallback);
+
+    server.on('error', function (e) {
+        if (e.code === 'EADDRINUSE') {
+            baboon.loggers.syslog.warn('Port ' + baboon.config.port + ' is already in use');
+            baboon.config.port++;
+
+            // try to start server again with other port
+            startServer();
+        } else {
+            throw e;
+        }
+    });
+}
+
+// start the server
+startServer();
 
 // Expose app
 module.exports = app;
